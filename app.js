289edf5dc7a6f962bb0bdcfc7240b8727f34cbb6
ahmedkhaledali1 <ahmed.khaled01233Egmail.com>
2025-08-14 11:58:10 +0300

    trust proxy

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
+
+app.set('trust proxy', 1);
+
 const morgan = require('morgan');
 const userRouter = require('./routes/user');
 const productRouter = require('./routes/products');
